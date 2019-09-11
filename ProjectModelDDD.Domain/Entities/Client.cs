dca2d076a85db0c13de4d228d8db0c9912d16af3
Paula Zaidel <paulazaaidel@gmail.com>
2019-09-11 19:22:01 -0300

    Created EntityBase.cs & Client.cs

---
+++ ProjectModelDDD.Domain/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ProjectModelDDD.Domain.Entities
+{
+    public class Client : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+        public bool Active { get; set; }
+        public bool SpecialClient(Client client)
+        {
+            return client.Active && DateTime.Now.Year - client.Created.Year >= 5;
+        }
+    }
+}
---
+++ ProjectModelDDD.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ProjectModelDDD.Domain.Entities
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+    }
+}
