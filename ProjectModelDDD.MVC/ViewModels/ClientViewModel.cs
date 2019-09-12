7b68342e894c0f9cb9d8e0df323f7110a97eea9e
Paula Zaidel <paulazaaidel@gmail.com>
2019-09-12 19:59:36 -0300

    Created ViewModels

---
+++ ProjectModelDDD.MVC/ViewModels/ClientViewModel.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectModelDDD.MVC.ViewModels
+{
+    public class ClientViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(150, ErrorMessage = "{0} characters max")]
+        [MinLength(2, ErrorMessage = "{0} characters minimum")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(150, ErrorMessage = "{0} characters max")]
+        [MinLength(2, ErrorMessage = "{0} characters minimum")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(100, ErrorMessage = "{0} characters max")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime Created { get; set; }
+
+        public bool Active { get; set; }
+
+        public virtual IEnumerable<ProductViewModel> Products { get; set; }
+    }
+}
---
+++ ProjectModelDDD.MVC/ViewModels/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectModelDDD.MVC.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(250, ErrorMessage = "{0} characters max")]
+        [MinLength(2, ErrorMessage = "{0} characters minimum")]
+        public string Name { get; set; }
+
+        [DataType(dataType: DataType.Currency)]
+        [Range(typeof(decimal), "0", "9999999999")]
+        [Required(ErrorMessage = "Required field")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Available?")]
+        public bool Available { get; set; }
+        public int ClientId { get; set; }
+        public virtual ClientViewModel Client { get; set; }
+    }
+}
